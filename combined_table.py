7d3593b438d6be96b2e7b5655ab3f6f187a20050
kaimin86 <kaimin.khaw@gmail.com>
2025-05-14 17:53:29 +0800

    your commit

--- combined_table.py
+++ combined_table.py
@@ -443,7 +443,7 @@ LS_gridOptions["getRowStyle"] = JsCode("""
 """)
 
 st.subheader("✅ Final table going into AgGrid:")
-st.dataframe(short_table_df[["short_name", "Raw Score", "Adjustment", "Adjusted Score", "Analyst Comment"]])
+st.dataframe(short_table_df[["short_name", "Adjustment", "Analyst Comment"]])
 
 ## Render the table using AgGrid (and pass in your overrides injecting the CSS injection)
 
